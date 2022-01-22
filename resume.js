2a334ade4360501a9a1e554a0190408ee50388b4
umamaheswarisiddhu <umakutty231292@gmail.com>
2022-01-22 09:00:49 +0530

    Create resume.js

---
+++ resume.js
@@ -0,0 +1,58 @@
+let resume = {
+    "basics": {
+      "name": "uma maheswari",
+      "label": "",
+      "picture": "",
+      "email": "umakutty231292@gmail.com",
+      "phone": "9675342134",
+      "degree": "B.E",
+      "location": {
+        "address": "iyyapan nagar 1st street",
+        "postalCode": "628002",
+        "city": "tamilnadu",
+        "countryCode": "+91",
+        "region": "hindu"
+    }
+},
+    "education": [
+      {
+        "institution": "knsk engineening college",
+        "area": "electronic communication engineering",
+        "startDate": "2.9.2010",
+        "endDate": "14.5.2014",
+         "courses": [
+          "B.E - ELECTRONIC COMMUNICATION ENGINEERING"
+        ]
+      }
+    ],
+    "skills": [
+      {
+        "name": "embedded,c++,python,java",
+        "level": "basic",
+        "keywords": [
+          "embedded","c++","python","java",
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "english",
+        "fluency": "english"
+      }
+    ],
+    "interests": [
+      {
+        "name": "indoor games",
+        "keywords": [
+          "chess"
+        ]
+      }
+  ]
+}
+console.log(resume);
+
+  
+
+  
+
+
